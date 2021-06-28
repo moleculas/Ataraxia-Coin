32c08fa2b223b19439d8df4d709ebd5be3eec107
Isaías <isaiasherreroflorensa@gmail.com>
2021-06-28 14:04:30 +0200

    3. Test completo

--- src/contracts/AtaraxiaToken.sol
+++ src/contracts/AtaraxiaToken.sol
@@ -7,7 +7,13 @@ contract AtaraxiaToken {
     string public standard = "Ataraxia Token v1.0";
     uint256 public totalSupply;
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     //constructor
     constructor(uint256 _initialSupply) public {
@@ -32,62 +38,34 @@ contract AtaraxiaToken {
         return true;
     }
 
-    // contract AtaraxiaToken {
-    //     string  public name = "Ataraxia Token";
-    //     string  public symbol = "ATRT";
-    //     string  public standard = "Ataraxia Token v1.0";
-    //     uint256 public totalSupply;
+    //transferencias delegadas
 
-    //     event Transfer(
-    //         address indexed _from,
-    //         address indexed _to,
-    //         uint256 _value
-    //     );
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
 
-    //     event Approval(
-    //         address indexed _owner,
-    //         address indexed _spender,
-    //         uint256 _value
-    //     );
-
-    //     mapping(address => uint256) public balanceOf;
-    //     mapping(address => mapping(address => uint256)) public allowance;
-
-    //    constructor (uint256 _initialSupply) {
-    //         balanceOf[msg.sender] = _initialSupply;
-    //         totalSupply = _initialSupply;
-    //     }
-
-    //     function transfer(address _to, uint256 _value) public returns (bool success) {
-    //         require(balanceOf[msg.sender] >= _value);
-
-    //         balanceOf[msg.sender] -= _value;
-    //         balanceOf[_to] += _value;
-
-    //         emit Transfer(msg.sender, _to, _value);
-
-    //         return true;
-    //     }
-
-    //     function approve(address _spender, uint256 _value) public returns (bool success) {
-    //         allowance[msg.sender][_spender] = _value;
-
-    //         emit Approval(msg.sender, _spender, _value);
-
-    //         return true;
-    //     }
-
-    //     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-    //         require(_value <= balanceOf[_from]);
-    //         require(_value <= allowance[_from][msg.sender]);
-
-    //         balanceOf[_from] -= _value;
-    //         balanceOf[_to] += _value;
-
-    //         allowance[_from][msg.sender] -= _value;
-
-    //         emit Transfer(_from, _to, _value);
-
-    //         return true;
-    //     }
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        //excepción si la cuenta _from no tiene suficiente
+        require(_value <= balanceOf[_from]);
+        //excepción si la asignación no es suficiente
+        require(_value <= allowance[_from][msg.sender]);
+        //cambiar el balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        //actualizar asignación
+        allowance[_from][msg.sender] -= _value;
+        //emitir evento
+        emit Transfer(_from, _to, _value);
+        //retorna booleano
+        return true;
+    }
 }
--- test/AtaraxiaToken.js
+++ test/AtaraxiaToken.js
@@ -37,8 +37,8 @@ contract('AtaraxiaToken', (accounts) => {
         }).then(assert.fail).catch((error) => {
             assert(error.message, 'el error message debe contener revert');
             return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] })
-        }).then((success)=> {
-            assert.equal(success, true, 'retorna true');            
+        }).then((success) => {
+            assert.equal(success, true, 'retorna true');
             return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
         }).then((receipt) => {
             assert.equal(receipt.logs.length, 1, 'dispara evento');
@@ -53,5 +53,65 @@ contract('AtaraxiaToken', (accounts) => {
         }).then((balance) => {
             assert.equal(balance.toNumber(), 750000, 'deduce el importe de la cuenta de envío');
         });
-    })
+    });
+    it('aprueba tokens para transferencia delegada', () => {
+        return AtaraxiaToken.deployed().then((instance) => {
+            tokenInstance = instance;
+            return tokenInstance.approve.call(accounts[1], 100);
+        }).then((success) => {
+            assert.equal(success, true, 'retorna true');
+            return tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+        }).then((receipt) => {
+            assert.equal(receipt.logs.length, 1, 'dispara evento');
+            assert.equal(receipt.logs[0].event, 'Approval', 'debe ser el "Approval" evento');
+            assert.equal(receipt.logs[0].args._owner, accounts[0], 'registra que la cuenta desde la que se transfieren los tokens esté autorizada');
+            assert.equal(receipt.logs[0].args._spender, accounts[1], 'registra que la cuenta a la que se transfieren los tokens esté autorizada');
+            assert.equal(receipt.logs[0].args._value, 100, 'registra la cantidad de la transferencia');
+            return tokenInstance.allowance(accounts[0], accounts[1]);
+        }).then((allowance) => {
+            assert.equal(allowance.toNumber(), 100, 'almacena la asignación para transferencia delegada');
+        });
+    });
+    it('maneja transferencias delegada de tokens', ()=> {
+        return AtaraxiaToken.deployed().then((instance)=> {
+            tokenInstance = instance;
+            fromAccount = accounts[2];
+            toAccount = accounts[3];
+            spendingAccount = accounts[4];
+            // Transferir algunos tokens a fromAccount
+            return tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
+        }).then((receipt)=> {
+            // Aprueba a spendingAccount gastar 10 tokens de fromAccount
+            return tokenInstance.approve(spendingAccount, 10, { from: fromAccount });
+        }).then((receipt) =>{
+            // Intenta transferir algo más grande que el saldo del remitente
+            return tokenInstance.transferFrom(fromAccount, toAccount, 9999, { from: spendingAccount });
+        }).then(assert.fail).catch((error)=> {
+            assert(error.message.indexOf('revert') >= 0, 'no se puede transferir un valor mayor que el saldo');
+            // Intenta transferir algo mayor que la cantidad aprobada
+            return tokenInstance.transferFrom(fromAccount, toAccount, 20, { from: spendingAccount });
+        }).then(assert.fail).catch((error) =>{
+            assert(error.message.indexOf('revert') >= 0, 'no se puede transferir un valor mayor que la cantidad aprobada');
+            return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, { from: spendingAccount });
+        }).then((success) =>{
+            assert.equal(success, true);
+            return tokenInstance.transferFrom(fromAccount, toAccount, 10, { from: spendingAccount });
+        }).then(function (receipt) {
+            assert.equal(receipt.logs.length, 1, 'dispara evento');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'debe ser el "Transfer" evento');
+            assert.equal(receipt.logs[0].args._from, fromAccount, 'registra la cuenta desde la que se transfieren los tokens');
+            assert.equal(receipt.logs[0].args._to, toAccount, 'registra la cuenta a la que se transfieren los tokens');
+            assert.equal(receipt.logs[0].args._value, 10, 'registra la cantidad de la transferencia');
+            return tokenInstance.balanceOf(fromAccount);
+        }).then((balance)=> {
+            assert.equal(balance.toNumber(), 90, 'deduce el importe de la cuenta de envío');
+            return tokenInstance.balanceOf(toAccount);
+        }).then((balance) =>{
+            assert.equal(balance.toNumber(), 10, 'suma la cantidad de la cuenta receptora');
+            return tokenInstance.allowance(fromAccount, spendingAccount);
+        }).then((allowance)=> {
+            assert.equal(allowance.toNumber(), 0, 'deduce la cantidad de la asignación');
+        });
+    });
+
 })
